91f438b44f55bb292d0e5055570b172597972061
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-09-06 16:22:39 -0700

    style app

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const background = document.querySelector('.background')
+const background = document.querySelector('body')
 const h2 = document.querySelector('h2')
+const name = document.getElementById('name')
+const type = document.getElementById('type')
+const lon = document.getElementById('lon')
+const lat = document.getElementById('lat')
+const temp = document.getElementById('temp')
 
 let city = "";
 
@@ -10,14 +15,30 @@ const img = document.querySelector('img');
 const weatherAPI = "371c44fb7c2d48cd04b154a7a724acf7";
 
 const changeBackground = (weatherData) => {
-  if (weatherData.sys.type === 1) {
+  console.log("background", weatherData.weather[0].main)
+  if (weatherData.weather[0].main == "Clear") {
     background.classList.add('night')
-  } else if (weatherData.sys.type === 2) {
+  } else if (weatherData.weather[0].main == 'Clouds') {    
     background.classList.add('day')
   } else {
     background.classList.add('morning')
   }
 }
+
+let celcius = 0;
+
+const kelvinToCelcius = (kelvin) => {
+  celcius = kelvin - 273.15
+}
+
+const updateWeather = (weatherData) => {
+  name.textContent = weatherData.name.toUpperCase()
+  temp.textContent = "Temperature: " + celcius.toFixed(2) + ' °'
+  type.textContent = "Description: " + weatherData.weather[0].main
+  lon.textContent = "Latitude: " + weatherData.coord.lon
+  lat.textContent = "Longitude: " + weatherData.coord.lat
+}
+
 
 const addGiph = (giph) => {
     img.src = giph
@@ -25,20 +46,24 @@ const addGiph = (giph) => {
 
 
 // fetch weather data on button click
-button.addEventListener('click', async function getWeather() {
+button.addEventListener('click', async function getWeather(e) {
+  e.preventDefault()
   city = input.value
 
   try {
     const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&limit=5&appid=${weatherAPI}`, {mode: 'cors'});
-    const weatherData = await weatherResponse.json();             
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData)             
     changeBackground(weatherData);
-
+    kelvinToCelcius(weatherData.main.temp)
+    
     const giphyResponse = await await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=V6DEqhbisIo51r2by8ps9KvlE6iWD2Fc&s=${city}`, {mode: 'cors'});
     const giphyData = await giphyResponse.json();
     addGiph(giphyData.data.images.original.url);
-
+    
     input.value = ""
     h2.textContent = ""
+    updateWeather(weatherData)
   } catch (error) {   
     h2.textContent = "City name not found";    
   } 
